14657762b91c27bf90a9ad3c27cd38f679ebf0e7
imranrasulzade <imranrasulzade5@gmail.com>
2023-04-30 02:31:35 +0400

    267 | Subtraction | Java

---
+++ Subtraction_267.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+        BigInteger a = new BigInteger(scan.next());
+        BigInteger b = new BigInteger(scan.next());
+        System.out.println(a.subtract(b));
+       
+    }
+}
